1f376353f05b90e36d827b64e82ff3a7ac5a38ad
Osvald osmo <osmo@itu.dk>
2026-02-04 15:49:37 +0100

    more css changes

--- script.js
+++ script.js
@@ -15,63 +15,73 @@ const startButton = document.getElementById('start-game-btn');
 
 const questions = [
     {
-        text: "Det her er det første spørgsmål, svaret er A",
-        explanation: "Forklaring på hvorfor A er korrekt",
+        text: "Hvilke bælgfrugter har flest danskere prøvet at spise?",
+        explanation: "De tre typer af bælgfrugter som flest danskere har prøvet at spise er edamamebønner, kikærter og kidneybønner. Under 5% har prøvet at spise sorterne ingridærter og gråærter.",
         options: [
-            { name: "A", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Edamamebønner", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
             { name: "B", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
             { name: "C", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "D", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+        ]
+    },
+    {
+        text: "Hvor mange danskere kender Fødevarestyrelsens officielle anbefalinger om, at man bør spise 100 gram bælgfrugter om dagen?",
+        explanation: "Fødevarestyrelsen anbefaler, at man spiser mindre kød, og mere fisk og bælgfrugter. De anbefaler, at man spiser ca. 100 gram bælgfrugter om dagen. Bælgfrugterne er både en god kilde til protein og andre næringsstoffer og er samtidig blandt de fødevarer, der har det laveste klimaaftryk.",
+        options: [
+            { name: "A", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "B", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "4% af befolkningen", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "D", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+        ]
+    },
+    {
+        text: "Hvor i Danmark er der flest, der spiser bælgfrugter?",
+        explanation: "I Region Hovedstaden er det over halvdelen, 52%, der spiser bælgfrugter ugentligt. I Region Midtjylland og Region Syddanmark er det 42%, i Region Sjælland 37% og i Region Nordjylland 32%",
+        options: [
+            { name: "Region Hovedstaden", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "B", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "C", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "D", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+        ]
+    },
+    {
+        text: "Spiser de danskere, der træner meget (4+ gange om ugen) bælgfrugter oftere eller sjældnere end resten?",
+        explanation: "Mens 14% af folk der træner under 4 gange om ugen har skiftet kødet ud med bælg inden for den seneste uge, er det hele 26% af de fysisk aktive",
+        options: [
+            { name: "Oftere", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Sjældnere", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Det samme", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
             //{ name: "D", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
         ]
     },
     {
-        text: "Det her er det næste spørgsmål, svaret er C",
-        explanation: "Forklaring på hvorfor C er korrekt",
-        options: [
-            { name: "A", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "B", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "C", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "D", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
-        ]
-    },
-    {
-        text: "Det her er endnu et spørgsmål, svaret er A",
-        explanation: "Forklaring på hvorfor A er korrekt",
-        options: [
-            { name: "A", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "B", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "C", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "D", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
-        ]
-    },
-    {
-        text: "Det her er det næstsidste spørgsmål, svaret er D",
+        text: "Hvilke kantiner er bedst til at inspirere os til at spise bælgfrugter?",
         explanation: "Forklaring på hvorfor D er korrekt",
         options: [
-            { name: "A", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "B", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "C", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "D", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+            { name: "Private", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Offentlige", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            //{ name: "C", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            //{ name: "D", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
         ]
     },
     {
-        text: "Det her er det sidste spørgsmål, svaret er D",
-        explanation: "Forklaring på hvorfor D er korrekt",
+        text: "Hvad er den største motivationsfaktor for danskerne til at spise flere bælgfrugter?",
+        explanation: "Sundhed er den største årsag til, at danskerne gerne vil spise flere bælgfrugter. Der er også store sundhedsfordele at komme efter. Bælgfrugterne har et højt fiberindhold, der har en positiv effekt på tarmmikrobiomet og fordøjelsen. Samtidig er de rige på protein, jern, zink, folsyre og kalium.",
         options: [
-            { name: "A", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "B", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "C", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "D", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+            { name: "Bælgfrugter er bedre for klimaet", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Bælgfrugter er sunde", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Bælgfrugter mætter godt", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Bælgfrugter er billigere", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
         ]
     },
     {
-        text: "okay et til, alle er rigtige",
-        explanation: "Forklaring på hvorfor alle er korrekte",
+        text: "Er danskerne bange for at få for få proteiner, hvis de erstatter kødet med bælgfrugter?",
+        explanation: "53% af danskerne svarer, at de er uenige i, at de er bange for ikke at få nok proteiner, hvis de i nogle måltider erstatter bælgfrugter med kød. Bælgfrugterne har også et højt proteinindhold. Eksempelvis indeholder kikærter 20% protein, røde linser 27% protein og sojabønner hele 36% protein",
         options: [
-            { name: "A", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "B", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "C", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
-            { name: "D", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+            { name: "Flertallet er bange", true: false, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            { name: "Flertallet er ikke bange", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            //{ name: "C", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
+            //{ name: "D", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
         ]
     },
 ];
