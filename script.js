ab119e5236d037bd2f2239b3995b7e2d2a4827ca
Osvald osmo <osmo@itu.dk>
2026-02-13 12:03:36 +0100

    word changes and grammar fixes

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const startButton = document.getElementById('start-game-btn');
 const questions = [
     {
         text: "Hvilke bælgfrugter har flest danskere prøvet at spise?",
-        explanation: "De tre bælgfruger, flest danskere har prøvet, er edamamebønner (60%), kikærter, (55%) og kidneybønner (53%), mens under 5% har prøvet ingridærter og gråærter",
+        explanation: "De tre bælgfruger, flest danskere har prøvet, er edamamebønner (60 %), kikærter (55 %) og kidneybønner (53 %), mens under 5 % har prøvet ingridærter og gråærter",
         options: [
             { name: "Edamamebønner", true: true, img: "./images/Edamame_bean.png" },
             { name: "Kikærter", true: false, img: "./images/Chickpea.png" },
@@ -28,15 +28,15 @@ const questions = [
         text: "Hvor mange danskere kender Fødevarestyrelsens officielle anbefalinger om, at man bør spise 100 gram bælgfrugter om dagen?",
         explanation: "Fødevarestyrelsen anbefaler, at spise mindre kød, mere fisk og flere bælgfrugter. De anbefaler, at man spiser 100 gram bælgfrugter om dagen. Bælgfrugterne er både en god kilde til protein og andre næringsstoffer og er samtidig blandt de fødevarer, der har det laveste klimaaftryk.",
         options: [
-            { name: "27% af befolkningen", true: false, img: "./images/27.png" },
-            { name: "17% af befolkningen", true: false, img: "./images/17.png" },
-            { name: "4% af befolkningen", true: true, img: "./images/4.png" },
-            { name: "1,5% af befolkningen", true: false, img: "./images/15.png"}
+            { name: "27 % af befolkningen", true: false, img: "./images/27.png" },
+            { name: "17 % af befolkningen", true: false, img: "./images/17.png" },
+            { name: "4 % af befolkningen", true: true, img: "./images/4.png" },
+            { name: "1,5 % af befolkningen", true: false, img: "./images/15.png"}
         ]
     },
     {
         text: "Hvor i Danmark er der flest, der spiser bælgfrugter?",
-        explanation: "I Region Hovedstaden er det over halvdelen (52%), der spiser bælgfrugter ugentligt. I Region Midtjylland og Region Syddanmark er det 42%, i Region Sjælland 37% og i Region Nordjylland 32%.",
+        explanation: "I Region Hovedstaden er det over halvdelen (52 %), der spiser bælgfrugter ugentligt. I Region Midtjylland og Region Syddanmark er det 42 %, i Region Sjælland 37 % og i Region Nordjylland 32 %.",
         options: [
             { name: "Region Hovedstaden", true: true, img: "https://upload.wikimedia.org/wikipedia/commons/6/6b/Map_DK_Region_Hovedstaden.png" },
             { name: "Region Midtjylland", true: false, img: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Map_DK_Region_Midtjylland.png" },
@@ -66,7 +66,7 @@ const questions = [
     },
     {
         text: "Hvad motiverer danskerne mest til at spise flere bælgfrugter?",
-        explanation: "Sundhed er den største motivation for at spise flere bælgfrugter – 88 % angiver det som den vigtigste årsag. Bælgfrugterne er rige på fiber, protein, jern, zink, folsyre og kalium, hvilket gavner både fordøjelsen og tarmfloraen. Herefter følger mæthed (61 %) og klimafordele (58 %).",
+        explanation: "Sundhed er den største motivation for at spise flere bælgfrugter – 88 % angiver det som den vigtigste årsag. Herefter følger mæthed (61 %) og klimafordele (58 %). Bælgfrugterne er rige på fiber, protein, jern, zink, folsyre og kalium, hvilket gavner både fordøjelsen og tarmfloraen.",
         options: [
             { name: "Bælgfrugter er bedre for klimaet", true: false, img: "./images/climate.png" },
             { name: "Bælgfrugter er sunde", true: true, img: "./images/healthy.png" },
@@ -76,10 +76,10 @@ const questions = [
     },
     {
         text: "Er danskerne bange for at få for lidt protein, hvis de skifter kødet ud med bælgfrugter?",
-        explanation: "53 % af danskerne svarer, at de er uenige i, at de er bange for ikke at få nok protein, hvis de i nogle måltider erstatter kød med bælgfrugter. Bælgfrugterne har også et højt proteinindhold i tørret vægt: kikærter indeholder 20%, røde linser 27% og sojabønner hele 36% protein.",
+        explanation: "53 % af danskerne svarer, at de er uenige i, at de er bange for ikke at få nok protein, hvis de i nogle måltider erstatter kød med bælgfrugter. Bælgfrugterne har også et højt proteinindhold i tørret vægt: kikærter indeholder 20 %, røde linser 27 % og sojabønner hele 36 % protein.",
         options: [
-            { name: "Flertallet er bange", true: false, img: "https://images.unsplash.com/photo-1583264277139-3d9682e44b03?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-            { name: "Flertallet er ikke bange", true: true, img: "https://images.unsplash.com/photo-1713947505435-b79c33c6c91a?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+            { name: "Flertallet er bange", true: false, img: "https://images.unsplash.com/photo-1583264277139-3d9682e44b03?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA %3D %3D" },
+            { name: "Flertallet er ikke bange", true: true, img: "https://images.unsplash.com/photo-1713947505435-b79c33c6c91a?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA %3D %3D" },
             //{ name: "C", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png" },
             //{ name: "D", true: true, img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
         ]
@@ -138,7 +138,7 @@ function renderExplanation() {
 
     const resultText = lastSelectedOption.true
         ? "Korrekt! Godt klaret."
-        : `Forkert. Det rigtige svar var <strong>${correctOption.name}</strong>.`;
+        : `Forkert. Det rigtige svar er <strong>${correctOption.name}</strong>.`;
 
     const lastQuestion = currentQuestion === questions.length - 1;
     const nextButtonText = lastQuestion ? "Se resultat" : "Næste spørgsmål";
